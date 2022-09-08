69e3544225713c621a414f8cd9bd8613a5cd8771
Yavuz Özbay <ozbayavz@gmail.com>
2022-09-08 19:19:10 +0200

    Updates

---
+++ precache-manifest.bcca9d0d381c5c640fceca73d967d7ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f62eeef7f6f14556a2a7",
+    "url": "/react-portfolio/static/css/main.c091e023.chunk.css"
+  },
+  {
+    "revision": "f62eeef7f6f14556a2a7",
+    "url": "/react-portfolio/static/js/main.f62eeef7.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/react-portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "f8926043fa4459ec0101",
+    "url": "/react-portfolio/static/js/runtime~main.f8926043.js"
+  },
+  {
+    "revision": "6a5bcb9340ec5136240a1b4809dbe0fe",
+    "url": "/react-portfolio/index.html"
+  }
+];
